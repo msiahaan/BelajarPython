776f28863b53094e226d5b8e4d03b005c74a3bc0
Mico Siahaan <mico.siahaan@gmail.com>
2023-09-08 22:44:14 +0800

    rename and edit slide 2, add dictionaty example

---
+++ contoh_dict.py
@@ -0,0 +1,11 @@
+straw_hat_pirates = {
+    'captain' : 'Monkey D. Luffy',
+    'navigator' : 'Nami',
+    'cook' : 'Sanji',
+    'doctor' : 'Chopper',
+    'swordman' : 'Zoro',
+    'arkeologist' : 'Robin'
+}
+print("Straw Hat Pirates: ")
+print("Captain: ", straw_hat_pirates['captain'])
+print("Navigator: ", straw_hat_pirates['navigator'])
