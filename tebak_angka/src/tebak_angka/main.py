d37467418130f2d857c88bce09679aa8733ef95c
Mico Siahaan <mico.siahaan@gmail.com>
2023-09-10 07:25:49 +0800

    deleted tebak.py to make it program simpler

--- tebak_angka/src/tebak_angka/main.py
+++ tebak_angka/src/tebak_angka/main.py
@@ -1,5 +1,20 @@
-from tebak import *
 from random import randint
+
+kesempatan = 7
+
+def print_art(teks):
+    from art import tprint
+    tprint(teks)
+
+def is_guessed(tebakan, angka):
+    if tebakan < angka:
+        print("Tebakan terlalu rendah!")
+    elif tebakan > angka:
+        print("Tebakan terlalu tinggi ")
+    else:
+        print()
+
+    return tebakan == angka
 
 def main():
     print_art("Tebak Angka")
