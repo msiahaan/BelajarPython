57844bc962d00c059a97bfef73b7758d354d1969
Mico Siahaan <mico.siahaan@gmail.com>
2023-09-18 15:39:44 +0800

    Add Hangman and modified .gitignore

---
+++ hangman/src/hangman_lib.py
@@ -0,0 +1,76 @@
+import random
+
+HANGMANPICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+#Word bank of animals
+words = ('ant baboon badger bat bear beaver camel cat clam cobra cougar '
+         'coyote crow deer dog donkey duck eagle ferret fox frog goat '
+         'goose hawk lion lizard llama mole monkey moose mouse mule newt '
+         'otter owl panda parrot pigeon python rabbit ram rat raven '
+         'rhino salmon seal shark sheep skunk sloth snake spider '
+         'stork swan tiger toad trout turkey turtle weasel whale wolf '
+         'wombat zebra ').split()
+
+class HangMan(object):
+    def __init__(self):
+        self.hangman_pics = HANGMANPICS
+        self.word_list = words
+        self.wordlist_len = len(self.word_list)
+
+    def get_random_word(self):
+        '''
+        This method return a random word
+
+        '''
+        idx = random.randint(0, self.wordlist_len - 1)
+        return self.word_list[idx]
+
