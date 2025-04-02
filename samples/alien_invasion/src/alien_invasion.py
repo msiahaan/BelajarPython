8eaa31b74c7551f3f816f908e21c474d72d52a9f
msiahaan <mico.siahaan@gmail.com>
2025-04-02 21:59:27 +0800

    Add Ship class and integrate into Alien Invasion game

--- samples/alien_invasion/src/alien_invasion.py
+++ samples/alien_invasion/src/alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -11,6 +12,7 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self) 
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -27,6 +29,7 @@ class AlienInvasion:
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
         pygame.display.flip()  # Make the most recently drawn screen visible.
 
 
