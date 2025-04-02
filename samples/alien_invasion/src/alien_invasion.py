df3c25a48af51ea93b0189b1325359d0d1e522bf
msiahaan <mico.siahaan@gmail.com>
2025-04-02 21:23:00 +0800

    Add Alien Invasion game implementation and settings

---
+++ samples/alien_invasion/src/alien_invasion.py
@@ -0,0 +1,36 @@
+import sys
+import pygame
+from settings import Settings
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+    def __init__(self):
+        """Initialize the game, and create resources."""
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            self._check_events()
+            self._update_screen()
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        pygame.display.flip()  # Make the most recently drawn screen visible.
+
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
