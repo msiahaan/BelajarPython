516ad721931c8283df9c6ae2cba1247cdf4c91fc
msiahaan <mico.siahaan@gmail.com>
2025-04-11 22:20:59 +0800

    Implement player movement controls with keyboard input

--- samples/space_invaders/player.py
+++ samples/space_invaders/player.py
@@ -26,8 +26,39 @@ def main_loop():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            # check for key presses
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    player.x_change = -5
+                if event.key == pygame.K_RIGHT:
+                    player.x_change = 5
+                if event.key == pygame.K_UP:
+                    player.y_change = -5
+                if event.key == pygame.K_DOWN:
+                    player.y_change = 5
+            # stop moving when key is released
+            if event.type == pygame.KEYUP:
+                if ((event.key == pygame.K_LEFT) or 
+					(event.key == pygame.K_RIGHT)):
+                    player.x_change = 0
+                if ((event.key == pygame.K_UP) or
+					(event.key == pygame.K_DOWN)):
+                    player.y_change = 0
 
         # RENDER YOUR GAME HERE
+        # Player movement
+        #if player.x <= 0:
+        #    player.x = 0
+        #if player.x >= 736:
+        #    player.x = 736
+
+        #if player.y <= 0:
+        #    player.y = 0
+        #if player.y >= 536:
+        #    player.y = 536
+
+        player.x += player.x_change
+        player.y += player.y_change
         # draw the player
         draw_player(player.img, player.x, player.y)
         # update the display
